f37024992e4551768d1ad25c75bb159c662e8c9a
handreysilva <handrey.silva@aluno.senai.br>
2026-03-23 20:59:48 -0400

    Primeiro Commit

---
+++ src/main/java/org/example/Carro.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Cargit ro {
+
+    // atributos
+    String marca;
+    String modelo;
+    int ano;
+
+    // método que apenas executa (não retorna nada)
+    void exibir() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+    }
+
+    // método que retorna um valor
+    int idadeDoCarro() {
+        return 2026 - ano;
+    }
+}
